89538a67092f2735e10569a1714d8a75dafb05b3
guayiyo89 <37852439+guayiyo89@users.noreply.github.com>
2019-07-19 11:13:49 -0400

    Update no-page-found.component.ts

--- src/app/pages/no-page-found/no-page-found.component.ts
+++ src/app/pages/no-page-found/no-page-found.component.ts
@@ -1,1 +1,30 @@
-lalal
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-page-found',
+  templateUrl: './no-page-found.component.html',
+  styles: []
+})
+export class NoPageFoundComponent implements OnInit {
+
+  frases = ['Ooops! No existe lo que andas buscando.','Pagina no encontada','¿Cómo llegastes acá?', 'Este es un Easter Egg', 'Tú estas aquí, nosotros no.', 'Parece que este no es un buen lugar.', 'Algo se perdió en el camino que no lo puedes encontrar']
+  max = this.frases.length;
+
+  lema = '';
+  constructor() { }
+
+  ngOnInit() {
+    this.generarlema();
+  }
+
+  generarlema(){
+    let num = this.numAleatorio(0,this.max);
+    this.lema = this.frases[num];
+    console.log(this.lema);
+    return this.lema;
+  }
+
+  numAleatorio(min, max){
+    return Math.floor(Math.random()*(max - min)) + min;
+  }
+}
