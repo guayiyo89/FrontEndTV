dfca6e3cf36fd9266d0ea3801a0dc176f552a1a8
Guayo <37852439+guayiyo89@users.noreply.github.com>
2019-07-26 12:56:35 -0400

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}
-];
+  {path: '', redirectTo: '/', pathMatch: 'full'}
+];;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
